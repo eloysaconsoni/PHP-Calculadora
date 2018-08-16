028f1d0225edeebe64ec2e2812fbd4afabe4218a
Eloysa Consoni <38021581+eloysaconsoni@users.noreply.github.com>
2018-08-15 21:34:16 -0300

    UPDATE php

--- calc.php
+++ calc.php
@@ -1,35 +1,52 @@
 <?php 
+//error_reporting(0);
 
-$num1 = $_POST["val1"]; 
-$oper = $_POST["opera"];
-$num2 = $_POST["val2"];
+ $_POST["val1"]; 
+ $_POST["opera"];
+ $_POST["val2"];
+teste();
 
-switch ($oper) {
-	case '+':$total =  $num1 + $num2;
+function teste(){
+if (is_null($_POST["val1"])) {
+	echo "Um dos valores não foi digitado, por favor verifique";
+}else{
+	caso();
+}
+}
+function caso(){
+switch ($_POST["opera"]) {
+	case '+':$total =  $_POST["val1"] + $_POST["val2"];
 		echo "O resultado da soma é: $total";
 		break;
 	
-	case '-': $total = $num1 - $num2;
+	case '-': $total = $_POST["val1"] - $_POST["val2"];
 			  echo "O resultado da subtração é: $total";
 		
 		break;	
 
-	case 'x': $total = $num1 * $num2;
+	case 'x': $total = $_POST["val1"] * $_POST["val2"];
 			  echo "O resultado da multiplicação é: $total";
 		
 		break;
-	case '*':$total = $num1 * $num2;
+	case '*':$total = $_POST["val1"] * $_POST["val2"];
 			  echo "O resultado da multiplicação é: $total";
 		
 		break;
-	case '/':$total = $num1 / $num2;
-			  echo "O resultado da divisão é: $total";
+	case '/':  if($_POST["val2"] == 0) {
+					echo "Não é possivel dividir por zero"; //validação de dividir por zero
+				}else{
+			  $total = $_POST["val1"] / $_POST["val2"];
+			  echo "O resultado da divisão é: $total";  }
 		
-		break;	
+		break;
+	case '%': $total = $_POST["val1"] * $_POST["val2"];
+			  $total = $total / 100;
+			  echo "O resultado da porcentagem é: $total";
+			break;
 	default:
-		# code...
+		
 		break;
 }
 
-
+}
  ?>
--- index.php
+++ index.php
@@ -3,10 +3,12 @@
 <head>
 	<title>Calculadora Sistemas</title>
 </head>
-<body>
-	<p> Calculadora em PHP </p>
+<body style=" background: #FFF0F5">
+	<link rel="stylesheet" type="text/css" href="css/css.css">
+	<p  style=""><h2><b class="fonte"> Calculadora em PHP</b></h2>  </p>
+	<p class="fonte" style="top: 50px">Calculadora feita com as opções que a linguagem PHP nos oferece</p>
 		<form  method="post" action="calc.php">
-	<fieldset>
+	<fieldset class="fundo">
 		<p>
 		<p><label for="pri">Digite o primeiro valor:</label><input id="pri" type="text" name="val1"></p>
 	   <p><label for="op">Digite a operação desejada:</label><input id="op" type="text" name="opera"></p>
